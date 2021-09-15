7878222c3149ebdcd2fdadd7dbb94ba7e67ffea1
dcronkite <dcronkite@gmail.com>
2021-09-15 08:33:16 -0700

    Changed default to syntok ssplit if available

--- src/runrex/text/ssplit.py
+++ src/runrex/text/ssplit.py
@@ -5,22 +5,28 @@ from loguru import logger
 try:
     from syntok import segmenter as syntok_segmenter
 except ImportError:
-    logger.warning('syntok not installed')
+    logger.warning('syntok not installed: defaulting to regex tokenizer')
     syntok_segmenter = False
 
 
-def default_ssplit(text: str, *, delim='\n') -> Tuple[str, int, int]:
+def regex_ssplit(text: str, *, delim='\n') -> Tuple[str, int, int]:
     start = 0
     for m in re.finditer(delim, text):
-        yield text[start:m.end()], start, m.end()
+        yield ' '.join(text[start:m.end()].split()), start, m.end()
         start = m.end()
-    yield text[start:], start, len(text)
+    yield ' '.join(text[start:].split()), start, len(text)
 
 
-def ssplit(text: str, ignore_newlines=True):
-    if ignore_newlines or not syntok_segmenter:
+def syntok_ssplit(text: str, ignore_newlines=True):
+    if ignore_newlines:
         # remove only single newlines, assume multiples are paragraph breaks
         text = ' '.join(re.split(r'(?<!\n)\n(?!\n)', text))
     for paragraph in syntok_segmenter.analyze(text):
         for sentence in paragraph:
             yield ' '.join(tok.value for tok in sentence)
+
+
+if syntok_segmenter:
+    default_ssplit = syntok_ssplit
+else:
+    default_ssplit = regex_ssplit
