852e7a04f22bc1057704c89d667ab5ce7e7c5372
dcronkite <dcronkite@gmail.com>
2021-11-11 10:54:04 -0800

    Bump to version 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import setuptools
 
 setup(name='runrex',
-      version='0.3.0',
+      version='0.3.1',
       description='Library to aid in organizing, running, and debugging regular expressions against'
                   ' a large body of text.',
       url='https://github.com/kpwhri/runrex',
