459d6a4fd346f79cebc8756f710970e875c135ac
dcronkite <dcronkite@gmail.com>
2023-02-01 14:40:30 -0800

    Refactored code from scripts to new library package

--- src/scripts/build_review_lists.py
+++ src/scripts/build_review_lists.py
@@ -3,20 +3,12 @@ Build lists of each variable (algorithm_CATEGORY) for reviewing/debugging.
 
 Currently, the offsets are not quite right, so a few different output options are provided.
 """
-import pathlib
+from runrex.cli.build_review_lists import build_review_lists_cli
 
-from runrex.post.build_review_lists import build_review_lists
+
+def main():
+    build_review_lists_cli()
+
 
 if __name__ == '__main__':
-    import argparse
-
-    parser = argparse.ArgumentParser(fromfile_prefix_chars='!@')
-    parser.add_argument('--output-file', dest='output_file', required=True, type=pathlib.Path,
-                        help='Output file for runrex. Either jsonl or csv.')
-    parser.add_argument('--log-file', dest='log_file', required=True, type=pathlib.Path,
-                        help='Loginfo file output by runrex. (jsonl format)')
-    parser.add_argument('--metafile', required=True, type=pathlib.Path,
-                        help='Fullpath to CSV file containing metadata. Must at least contain:'
-                             ' doc_id, text. May also contain'
-                             ' other variables as well.')
-    build_review_lists(**vars(parser.parse_args()))
+    main()
